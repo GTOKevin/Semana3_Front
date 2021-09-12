4706d8d11daa6e9642bc6a1f0be760e2993707fc
virgilio_Kevin <virgiliorojaskevinalexander@gmail.com>
2021-09-11 21:55:20 -0500

    Solucion

---
+++ src/app/components/add-alumno/add-alumno.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Alumno } from 'src/app/models/alumno.model';
+import { AlumnoService } from 'src/app/services/alumno.service';
+
+
+@Component({
+  selector: 'app-add-alumno',
+  templateUrl: './add-alumno.component.html',
+  styleUrls: ['./add-alumno.component.css']
+})
+export class AddAlumnoComponent implements OnInit {
+
+  constructor(private alumnoService: AlumnoService) { }
+
+  alumno : Alumno={
+    idAlumno: 0,
+    nombre: '',
+    dni: '',
+    correo: '',
+    fechaNacimiento: new Date()
+  }
+
+  ngOnInit(): void {
+  }
+
+  saveAlumno():void{
+
+    console.log(this.alumno);
+
+    this.alumnoService.create(this.alumno).subscribe(
+      response=>{
+        console.log(response.mensaje);
+        console.log(response);
+      },error=>{
+        console.log(console.error);
+        
+      }
+    )
+  }
+  
+
+}
